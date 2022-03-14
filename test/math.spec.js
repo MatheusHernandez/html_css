b2d50a5ebd459ad2ddf64e712eb7561e802b4ec3
MatheusHernandez <matheushdz0903@gmail.com>
2022-03-14 18:23:08 -0300

    master

--- test/math.spec.js
+++ test/math.spec.js
@@ -2,6 +2,8 @@ const assert= require('assert')
 const Math = require('../src/math.js')
 
 describe('Math class', function(){
+    throw new Error('Oh no!')
+    
     it('Sum two numbers', function(){
         const math = new Math()
 
