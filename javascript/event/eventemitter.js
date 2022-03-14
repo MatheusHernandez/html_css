df0486efdcdb8342f25e737734e25ff58b70a90e
MatheusHernandez <matheushdz0903@gmail.com>
2022-03-14 12:39:43 -0300

    master

---
+++ javascript/event/eventemitter.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('User logged', data => {
+    console.log(data)
+})
+
+emitter.emit('User logged', { user: 'Matheus'})
---
+++ test/math.spec.js
@@ -0,0 +1,11 @@
+const assert= require('assert')
+const Math = require('../src/math.js')
+
+describe('Math class', function(){
+    it('Sum two numbers', function(){
+        const math = new Math()
+
+        throw new Error('Oh no!')
+        assert.equal(math.sum(5,5), 10)
+    })
+})
